e742749eddb95dc36ae3e51fecef631567ec7d12
Ahmed-Taha-Dahmou <friv14666@gmail.com>
2024-06-18 02:14:35 +0100

    updated the like and dislike buttons working good

--- django_cotex/comments/views.py
+++ django_cotex/comments/views.py
@@ -2,8 +2,11 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from .models import Comment
 from .serializers import CommentDetailSerializer, CommentSerializer
+from .models import Comment, LikeDislike
+
+
+
 
 class CommentListAPIView(generics.ListAPIView):
     serializer_class = CommentDetailSerializer
@@ -23,3 +26,64 @@ class CommentCreateAPIView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)  # Automatically set the user based on the request
+
+
+
+
+class LikeCommentAPIView(generics.UpdateAPIView):
+    serializer_class = CommentDetailSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def update(self, request, *args, **kwargs):
+        user = request.user
+        comment_id = self.kwargs.get('comment_id')
+
+        try:
+            comment = Comment.objects.get(pk=comment_id)
+        except Comment.DoesNotExist:
+            return Response({"error": "Comment does not exist"}, status=status.HTTP_404_NOT_FOUND)
+
+        like, created = LikeDislike.objects.get_or_create(user=user, comment=comment)
+
+        # If already liked, remove the like
+        if like.like:
+            like.delete()
+        else:
+            like.like = True
+            like.save()
+
+        serializer = self.get_serializer(comment)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class DislikeCommentAPIView(generics.UpdateAPIView):
+    serializer_class = CommentDetailSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def update(self, request, *args, **kwargs):
+        user = request.user
+        comment_id = self.kwargs.get('comment_id')
+
+        try:
+            comment = Comment.objects.get(pk=comment_id)
+        except Comment.DoesNotExist:
+            return Response({"error": "Comment does not exist"}, status=status.HTTP_404_NOT_FOUND)
+
+        # Check if the user has already disliked the comment
+        dislike, created = LikeDislike.objects.get_or_create(user=user, comment=comment)
+
+        if created or dislike.like:
+            # If the dislike is new or the user previously liked the comment
+            dislike.like = False
+            dislike.save()
+        else:
+            # If the user already disliked the comment, remove the dislike
+            dislike.delete()
+
+        # Update the comment object with the new dislikes count
+        comment.refresh_from_db()
+
+        serializer = self.get_serializer(comment)
+        return Response(serializer.data, status=status.HTTP_200_OK)
--- react_cotex/src/components/CommentSection/CommentSection.js
+++ react_cotex/src/components/CommentSection/CommentSection.js
@@ -5,7 +5,7 @@ import './CommentSection.css';
 import LoginForm from '../LoginForm/LoginForm';
 import LikeButton from './LikeButton';
 import DislikeButton from './DislikeButton';
-import ReplyForm from './ReplyForm'; // Import the ReplyForm component
+import ReplyForm from './ReplyForm';
 import config from '../../config';
 
 const CommentSection = ({ gameId }) => {
@@ -13,31 +13,121 @@ const CommentSection = ({ gameId }) => {
   const [comments, setComments] = useState([]);
   const [newComment, setNewComment] = useState('');
   const [showLogin, setShowLogin] = useState(false);
-  const [replyingTo, setReplyingTo] = useState(null); // Track which comment we are replying to
+  const [replyingTo, setReplyingTo] = useState(null);
+  const [likedComments, setLikedComments] = useState(new Set());
+  const [dislikedComments, setDislikedComments] = useState(new Set());
+
+  const fetchComments = async () => {
+    const accessToken = sessionStorage.getItem('accessToken');
+    const headers = {};
+
+    if (accessToken) {
+      headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    try {
+      const response = await axios.get(`${config.API_URL}/comments/?game=${gameId}`, {
+        headers: headers,
+      });
+
+      const fetchedComments = response.data.results;
+      setComments(fetchedComments);
+
+      const initialLikedComments = new Set();
+      const initialDislikedComments = new Set();
+
+      fetchedComments.forEach(comment => {
+        if (comment.liked_disliked_by_user === true) {
+          initialLikedComments.add(comment.id);
+        } else if (comment.liked_disliked_by_user === false) {
+          initialDislikedComments.add(comment.id);
+        }
+      });
+
+      setLikedComments(initialLikedComments);
+      setDislikedComments(initialDislikedComments);
+
+    } catch (error) {
+      console.error('Error fetching comments:', error);
+    }
+  };
 
   useEffect(() => {
-    const fetchComments = async () => {
-      try {
-        const response = await axios.get(`${config.API_URL}/comments/?game=${gameId}`);
-        setComments(response.data.results);
-      } catch (error) {
-        console.error('Error fetching comments:', error);
-      }
-    };
-
     fetchComments();
   }, [gameId]);
 
-  const handleLikeToggle = (commentId) => {
-    // Handle like toggle logic here
+  const handleLikeToggle = async (commentId) => {
+    try {
+      const accessToken = sessionStorage.getItem('accessToken');
+      await axios.put(
+        `${config.API_URL}/comments/${commentId}/like/`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      setLikedComments((prevLikedComments) => {
+        const newLikedComments = new Set(prevLikedComments);
+        if (newLikedComments.has(commentId)) {
+          newLikedComments.delete(commentId);
+        } else {
+          newLikedComments.add(commentId);
+          setDislikedComments((prevDislikedComments) => {
+            const newDislikedComments = new Set(prevDislikedComments);
+            newDislikedComments.delete(commentId);
+            return newDislikedComments;
+          });
+        }
+        return newLikedComments;
+      });
+      // Call fetchComments or any other function to update comments
+      fetchComments();
+    } catch (error) {
+      console.error('Error toggling like:', error);
+    }
   };
 
-  const handleDislikeToggle = (commentId) => {
-    // Handle dislike toggle logic here
+  const handleDislikeToggle = async (commentId) => {
+    try {
+      const accessToken = sessionStorage.getItem('accessToken');
+      await axios.put(
+        `${config.API_URL}/comments/${commentId}/dislike/`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+  
+      setDislikedComments((prevDislikedComments) => {
+        const newDislikedComments = new Set(prevDislikedComments);
+        if (newDislikedComments.has(commentId)) {
+          newDislikedComments.delete(commentId);
+        } else {
+          newDislikedComments.add(commentId);
+          // Remove from likedComments if it was liked before
+          setLikedComments((prevLikedComments) => {
+            const newLikedComments = new Set(prevLikedComments);
+            newLikedComments.delete(commentId);
+            return newLikedComments;
+          });
+        }
+        return newDislikedComments;
+      });
+  
+      // Call fetchComments or any other function to update comments
+      fetchComments();
+    } catch (error) {
+      console.error('Error toggling dislike:', error);
+    }
   };
 
   const handleCommentSubmit = async (e) => {
-    e.preventDefault(); // Prevent the default form submission behavior
+    e.preventDefault();
     if (!isLoggedIn()) {
       setShowLogin(true);
       return;
@@ -46,7 +136,7 @@ const CommentSection = ({ gameId }) => {
     try {
       const accessToken = sessionStorage.getItem('accessToken');
       const response = await axios.post(
-        'http://localhost:8000/comments/create/',
+        `${config.API_URL}/comments/create/`,
         {
           user: user.id,
           game: gameId,
@@ -61,7 +151,7 @@ const CommentSection = ({ gameId }) => {
       );
 
       const newCommentData = processCommentData(response.data);
-      setComments([...comments, newCommentData]);
+      setComments([newCommentData, ...comments]);
       setNewComment('');
     } catch (error) {
       console.error('Error adding comment:', error);
@@ -77,7 +167,7 @@ const CommentSection = ({ gameId }) => {
     try {
       const accessToken = sessionStorage.getItem('accessToken');
       const response = await axios.post(
-        'http://localhost:8000/comments/create/',
+        `${config.API_URL}/comments/create/`,
         {
           user: user.id,
           game: gameId,
@@ -90,118 +180,144 @@ const CommentSection = ({ gameId }) => {
           },
         }
       );
-
-      const newCommentData = processCommentData(response.data);
-      setComments([...comments, newCommentData]);
-      setReplyingTo(null);
-    } catch (error) {
-      console.error('Error replying to comment:', error);
-    }
-  };
-
-  const handleCloseLogin = () => {
-    setShowLogin(false);
-  };
-
-  const handleReply = (commentId) => {
-    setReplyingTo(commentId);
-  };
-
-  const handleCancelReply = () => {
-    setReplyingTo(null);
-  };
-
-  const processCommentData = (commentData) => {
-    const profilePicUri = localStorage.getItem('profile_pic');
-    const profilePicUrl = `http://localhost:8000/${profilePicUri}`;
-    const username = localStorage.getItem('username');
-    const currentTime = new Date().toISOString();
-    return {
-      ...commentData,
-      user: {
-        ...commentData.user,
-        profile_pic: profilePicUrl,
-        username: username,
-      },
-      time: currentTime,
+  
+        const newCommentData = processCommentData(response.data);
+        setComments([newCommentData, ...comments]);
+        setReplyingTo(null);
+      } catch (error) {
+        console.error('Error replying to comment:', error);
+      }
     };
-  };
-
-  const renderMainComments = () => {
-    return comments
-      .filter((comment) => !comment.parent)
-      .map((comment) => (
-        <li key={comment.id} className="comment-item">
-          {/* Render main comment */}
-          <div className="comment-info">
-            <div className="user-avatar">
-              <img src={comment.user.profile_pic} alt="Profile" />
+  
+    const handleCloseLogin = () => {
+      setShowLogin(false);
+    };
+  
+    const handleReply = (commentId) => {
+      setReplyingTo(commentId);
+    };
+  
+    const handleCancelReply = () => {
+      setReplyingTo(null);
+    };
+  
+    const processCommentData = (commentData) => {
+      const profilePicUri = localStorage.getItem('profile_pic');
+      const profilePicUrl = `${config.API_URL}/${profilePicUri}`;
+      const username = localStorage.getItem('username');
+      const currentTime = new Date().toISOString();
+      return {
+        ...commentData,
+        user: {
+          ...commentData.user,
+          profile_pic: profilePicUrl,
+          username: username,
+        },
+        time: currentTime,
+      };
+    };
+  
+    const renderMainComments = () => {
+      return comments
+        .filter((comment) => !comment.parent)
+        .map((comment) => (
+          <li key={comment.id} className="comment-item">
+            <div className="comment-info">
+              <div className="user-avatar">
+                <img src={comment.user.profile_pic} alt="Profile" />
+              </div>
+              <div className="user-details">
+                <strong className="user-username">{comment.user.username}</strong>
+                <span className="comment-time">{new Date(comment.time).toLocaleString()}</span>
+              </div>
             </div>
-            <div className="user-details">
-              <strong className="user-username">{comment.user.username}</strong>
-              <span className="comment-time">{new Date(comment.time).toLocaleString()}</span>
+            <div className="comment-content">
+              <p>{comment.text}</p>
+              <div className="reply-button" onClick={() => handleReply(comment.id)}>
+                Reply
+              </div>
             </div>
-          </div>
-          <div className="comment-content">
-            <p>{comment.text}</p>
-            <div className="reply-button" onClick={() => handleReply(comment.id)}>
-              Reply
+            <div className="comment-actions">
+              <div className="like-container">
+              <LikeButton
+                commentId={comment.id}
+                handleLikeToggle={handleLikeToggle}
+                isLiked={likedComments.has(comment.id)}
+                likesCount={comment.likes_count}
+                updateLikesCount={fetchComments} // Pass fetchComments or any other function to update likes count
+              />
+              <DislikeButton
+                commentId={comment.id}
+                handleDislikeToggle={handleDislikeToggle}
+                isDisliked={dislikedComments.has(comment.id)}
+                dislikesCount={comment.dislikes_count}
+                updateDislikesCount={fetchComments} // Pass fetchComments or any other function to update dislikes count
+              />
+              </div>
             </div>
-          </div>
-          {/* Render like and dislike buttons */}
-          <LikeButton commentId={comment.id} handleLikeToggle={handleLikeToggle} isLiked={comment.isLiked} />
-          <DislikeButton commentId={comment.id} handleDislikeToggle={handleDislikeToggle} isDisliked={comment.isDisliked} />
-          {/* Render replies */}
-          <ul className="replies">
-            {renderReplyComments(comment.id)}
-          </ul>
-          {/* Render reply form */}
-          {replyingTo === comment.id && (
-            <ReplyForm onSubmit={(text) => handleCommentReply(text, comment.id)} onCancel={handleCancelReply} />
-          )}
-        </li>
-      ));
-  };
-
-  const renderReplyComments = (parentId) => {
-    return comments
-      .filter((comment) => comment.parent === parentId)
-      .map((comment) => (
-        <li key={comment.id} className="comment-item">
-          {/* Render reply comment */}
-          <div className="comment-info">
-            <div className="user-avatar">
-              <img src={comment.user.profile_pic} alt="Profile" />
+            <ul className="replies">
+              {renderReplyComments(comment.id)}
+            </ul>
+            {replyingTo === comment.id && (
+              <ReplyForm onSubmit={(text) => handleCommentReply(text, comment.id)} onCancel={handleCancelReply} />
+            )}
+          </li>
+        ));
+    };
+  
+    const renderReplyComments = (parentId) => {
+      return comments
+        .filter((comment) => comment.parent === parentId)
+        .map((comment) => (
+          <li key={comment.id} className="comment-item">
+            <div className="comment-info">
+              <div className="user-avatar">
+                <img src={comment.user.profile_pic} alt="Profile" />
+              </div>
+              <div className="user-details">
+                <strong className="user-username">{comment.user.username}</strong>
+                <span className="comment-time">{new Date(comment.time).toLocaleString()}</span>
+              </div>
             </div>
-            <div className="user-details">
-              <strong className="user-username">{comment.user.username}</strong>
-              <span className="comment-time">{new Date(comment.time).toLocaleString()}</span>
+            <div className="comment-content">
+              <p>{comment.text}</p>
+              <div className="reply-button" onClick={() => handleReply(comment.id)}>
+                Reply
+              </div>
+              <div className="like-wrapper">
+                <div className="like-container">
+                  <span className="like-count">{comment.likes_count}</span>
+                  <LikeButton
+                    commentId={comment.id}
+                    handleLikeToggle={handleLikeToggle}
+                    isLiked={likedComments.has(comment.id)}
+                    updateLikesCount={fetchComments} // Pass fetchComments or any other function to update likes count
+                  />
+                </div>
+                <div className="dislike-container">
+                  <span className="dislike-count">{comment.dislikes_count}</span>
+                  <DislikeButton
+                    commentId={comment.id}
+                    handleDislikeToggle={handleDislikeToggle}
+                    isDisliked={dislikedComments.has(comment.id)}
+                    updateDislikesCount={fetchComments} // Pass fetchComments or any other function to update dislikes count
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="comment-content">
-            <p>{comment.text}</p>
-            <div className="reply-button" onClick={() => handleReply(comment.id)}>
-              Reply
-            </div>
-          </div>
-          {/* Render like and dislike buttons */}
-          <LikeButton commentId={comment.id} handleLikeToggle={handleLikeToggle} isLiked={comment.isLiked} />
-          <DislikeButton commentId={comment.id} handleDislikeToggle={handleDislikeToggle} isDisliked={comment.isDisliked} />
-          {/* Render nested replies */}
-          <ul className="replies">
-            {renderReplyComments(comment.id)}
-          </ul>
-          {/* Render reply form */}
-          {replyingTo === comment.id && (
-            <ReplyForm onSubmit={(text) => handleCommentReply(text, comment.id)} onCancel={handleCancelReply} />
-          )}
-        </li>
-      ));
-  };
-
-  const renderCommentForm = () => {
-    if (!isLoggedIn()) {
-      return (
+            <ul className="replies">
+              {renderReplyComments(comment.id)}
+            </ul>
+            {replyingTo === comment.id && (
+              <ReplyForm onSubmit={(text) => handleCommentReply(text, comment.id)} onCancel={handleCancelReply} />
+            )}
+          </li>
+        ));
+    };
+    
+  
+    const renderCommentForm = () => {
+      if (!isLoggedIn()) {       return (
         <div className="login-required">
           Please <button onClick={() => setShowLogin(true)}>login</button> to comment.
         </div>
@@ -223,10 +339,10 @@ const CommentSection = ({ gameId }) => {
   return (
     <div className="comment-section">
       <h2 style={{ textAlign: 'center', marginBottom: '20px' }}>Comments</h2>
+      {renderCommentForm()}
       <ul className="comment-list">
         {renderMainComments()}
       </ul>
-      {renderCommentForm()}
       {showLogin && (
         <div className="login-modal">
           <div className="login-modal-content">
@@ -240,3 +356,5 @@ const CommentSection = ({ gameId }) => {
 };
 
 export default CommentSection;
+
+  
