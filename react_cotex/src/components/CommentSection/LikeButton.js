d9e89864f33a75e36b6b862556de342a66ac51cb
AH-TA-DA <55698715+Ahmed-Taha-Dahmoul@users.noreply.github.com>
2024-08-20 18:48:32 +0100

    update like dislike buttons

--- react_cotex/src/components/CommentSection/LikeButton.js
+++ react_cotex/src/components/CommentSection/LikeButton.js
@@ -9,7 +9,7 @@ const LikeButton = ({ commentId, handleLikeToggle, isLiked, likesCount, updateLi
   };
 
   return (
-    <div className={`like-container ${isLiked ? 'liked' : ''}`} onClick={handleLikeClick}>
+    <div className={`like-container-1 ${isLiked ? 'liked' : ''}`} onClick={handleLikeClick}>
       <input type="checkbox" checked={isLiked} readOnly />
       <svg id="LikeGlyph" version="1.1" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
         <path d="M29.845,17.099l-2.489,8.725C26.989,27.105,25.804,28,24.473,28H11c-0.553,0-1-0.448-1-1V13  c0-0.215,0.069-0.425,0.198-0.597l5.392-7.24C16.188,4.414,17.05,4,17.974,4C19.643,4,21,5.357,21,7.026V12h5.002  c1.265,0,2.427,0.579,3.188,1.589C29.954,14.601,30.192,15.88,29.845,17.099z" />
